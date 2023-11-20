4d90640a843f5fb35de17f4918fd54e28e6c4854
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-20 14:39:45 -0500

    Add new routes for forms

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterPagesComponent } from './pages/register-pages/register-pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		children: [
+			{ path: 'sign-up', component: RegisterPagesComponent },
+			{ path: '**', redirectTo: 'sign-up' },
+		]
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
 export class AuthRoutingModule { }
