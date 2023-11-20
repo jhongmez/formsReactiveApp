55407db21fa929db75030cef548a56d6d06a6eeb
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-20 15:51:26 -0500

    Add form basic

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
-  templateUrl: './basic-page.component.html',
-  styles: [
-  ]
+	templateUrl: './basic-page.component.html',
+	styles: [
+	]
 })
 export class BasicPageComponent {
+	
+	// public myForm: FormGroup = new FormGroup({
+	// 	name: new FormControl(''),
+	// 	price: new FormControl(0),
+	// 	inStorage: new FormControl(0)
+	// })
 
+	public myForm: FormGroup = this.formBuilder.group({
+		name: 		['', [ Validators.required, Validators.minLength(3) ]],
+		price: 		[0, [ Validators.required, Validators.min(0) ]],
+		inStorage: 	[0, [ Validators.required, Validators.min(0) ]]
+	})
+
+	constructor( private formBuilder: FormBuilder ) {}
+
+	onSave() {
+		if (this.myForm.invalid) return;
+		console.log(this.myForm.value);
+	}
 }
