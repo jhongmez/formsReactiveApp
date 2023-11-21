a99f3ce0f0b5d9b3f7ab52cf17d1b92816ac965d
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-21 12:14:26 -0500

    Add form array and delete

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
+	templateUrl: './dynamic-page.component.html',
+	styles: [
+	]
 })
 export class DynamicPageComponent {
+   
+   public myForm: FormGroup = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      favoriteGames: this.formBuilder.array([
+         ['Metal Gear', Validators.required],
+         ['League of legends', Validators.required],
+      ])
+   })
+
+   constructor( private formBuilder: FormBuilder ) {}
+
+   get favoriteGames() {
+      return this.myForm.get('favoriteGames') as FormArray;
+   }
+
+   isValidField( field: string ): boolean | null {
+		return this.myForm.controls[field].errors 
+         && this.myForm.controls[field].touched
+	}
+
+   isValidFieldInArray ( formArray: FormArray, i: number) {
+      return formArray.controls[i].errors 
+         && formArray.controls[i].touched
+   }
+
+	getFieldError( field: string ): string | null {
+		
+		if( !this.myForm.controls[field] ) return null;
+
+		const errors = this.myForm.controls[field].errors || {};
+
+		for (const key of Object.keys(errors)) {
+			switch( key ) {
+				case 'required': 
+					return 'Este campo es requerido';
+				case 'minlength':
+					return `Este campo requiere minimo ${ errors['minlength'].requiredLength } letras`;
+			}
+		}
+
+		return null;
+
+	}
+
+   onDeleteFavorite( index: number ):void {
+      this.favoriteGames.removeAt( index );
+   }
+
+   onSubmit() {
+      if( this.myForm.invalid ) {
+         this.myForm.markAllAsTouched();
+			return;
+      }
+      console.log(this.myForm.value);
+      this.myForm.reset();
+   }
 
 }
